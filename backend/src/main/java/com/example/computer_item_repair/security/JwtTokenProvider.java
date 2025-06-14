ccf566f14cc68cd7b8ab0463035d91a2fce04dc4
Yasindu20 <yasindudemel@gmail.com>
2025-06-14 11:05:08 +0530

    server error-2

--- backend/src/main/java/com/example/computer_item_repair/security/JwtTokenProvider.java
+++ backend/src/main/java/com/example/computer_item_repair/security/JwtTokenProvider.java
@@ -4,7 +4,6 @@ import com.example.computer_item_repair.domain.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-// Import the security package version of SignatureException
 import io.jsonwebtoken.security.SignatureException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
